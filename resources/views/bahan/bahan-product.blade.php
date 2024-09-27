58d424ccc3c69fee196246c020f80ef06f9326c6
Galih Titis BC <hhonggil007@gmail.com>
2024-09-27 19:42:12 +0700

    fix button

--- resources/views/bahan/bahan-product.blade.php
+++ resources/views/bahan/bahan-product.blade.php
@@ -13,10 +13,14 @@
             @csrf
             <div class="card-body ms-5">
                 <div class="bahan-container">
+                    <div class="text-center my-4">
+                        <button type="button" class="btn btn-success" data-toggle="modal"
+                            data-target="#tambah-satuan">Tambah Satuan Bahan</button>
+                    </div>
                     @if (old('bahan_id'))
                         @foreach (old('bahan_id') as $index => $oldBahanId)
                             <div class="row d-flex flex-wrap justify-content-center bahan-row">
-                                <div class="col-lg-3 col-sm-1 col-md-1">
+                                <div class="col-lg-3 col-sm-1 col-md-3">
                                     <div class="form-group">
                                         <label for="bahan_id">Bahan : </label>
                                         <select class="form-control bahan-select" name="bahan_id[]">
@@ -33,7 +37,7 @@
                                         @endif
                                     </div>
                                 </div>
-                                <div class="col-lg-3 col-sm-1 col-md-1">
+                                <div class="col-lg-3 col-sm-1 col-md-3">
                                     <div class="form-group">
                                         <label for="satuan_bahan">Satuan Bahan : </label>
                                         <select name="satuan_bahan[]" class="form-control" id="">
@@ -49,23 +53,30 @@
                                         @endif
                                     </div>
                                 </div>
-                                <div class="col-lg-3 col-sm-1 col-md-1">
-                                    <div class="form-group">
-                                        <label for="takaran">Takaran: </label>
-                                        <input type="text" name="takaran[]" class="form-control"
-                                            placeholder="Masukkan Takaran" value="{{ old('takaran.' . $index) }}">
-                                        @if ($errors->has('takaran.' . $index))
-                                            <span class="text-danger">{{ $errors->first('takaran.' . $index) }}</span>
-                                        @endif
+                                <div class="col-lg-3 col-sm-1 col-md-3">
+                                    <label for="takaran">Takaran: </label>
+                                    <div class="row">
+                                        <div class="col-lg-9 col-md-9">
+                                            <div class="form-group">
+                                                <input type="text" name="takaran[]" class="form-control"
+                                                    placeholder="Masukkan Takaran" value="{{ old('takaran.' . $index) }}">
+                                                @if ($errors->has('takaran.' . $index))
+                                                    <span
+                                                        class="text-danger">{{ $errors->first('takaran.' . $index) }}</span>
+                                                @endif
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-3 col-md-3">
+                                            <button type="button" class="remove-bahan btn btn-danger"><i
+                                                    class="fa fa-trash" aria-hidden="true"></i></button>
+                                        </div>
                                     </div>
                                 </div>
-                                <button type="button" class="remove-bahan btn btn-danger"><i class="fa fa-trash"
-                                        aria-hidden="true"></i></button>
                             </div>
                         @endforeach
                     @else
                         <div class="row d-flex flex-wrap justify-content-center bahan-row">
-                            <div class="col-lg-3 col-sm-1 col-md-1">
+                            <div class="col-lg-3 col-sm-1 col-md-3">
                                 <div class="form-group">
                                     <label for="bahan_id">Bahan : </label>
                                     <select class="form-control bahan-select" name="bahan_id[]">
@@ -79,7 +90,7 @@
                                     @endif
                                 </div>
                             </div>
-                            <div class="col-lg-3 col-sm-1 col-md-1">
+                            <div class="col-lg-3 col-sm-1 col-md-3">
                                 <div class="form-group">
                                     <label for="satuan_bahan">Satuan Bahan : </label>
                                     <select name="satuan_bahan[]" class="form-control" id="">
@@ -95,18 +106,24 @@
                                     @endif
                                 </div>
                             </div>
-                            <div class="col-lg-3 col-sm-1 col-md-1">
-                                <div class="form-group">
-                                    <label for="takaran">Takaran: </label>
-                                    <input type="text" name="takaran[]" class="form-control"
-                                        placeholder="Masukkan Takaran">
-                                    @if ($errors->has('takaran.0'))
-                                        <span class="text-danger">{{ $errors->first('takaran.0') }}</span>
-                                    @endif
+                            <div class="col-lg-3 col-sm-1 col-md-3">
+                                <label for="takaran">Takaran: </label>
+                                <div class="row">
+                                    <div class="col-lg-9 col-md-9">
+                                        <div class="form-group">
+                                            <input type="text" name="takaran[]" class="form-control"
+                                                placeholder="Masukkan Takaran">
+                                            @if ($errors->has('takaran.0'))
+                                                <span class="text-danger">{{ $errors->first('takaran.0') }}</span>
+                                            @endif
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-3 col-md-3">
+                                        <button type="button" class="remove-bahan btn btn-danger"><i class="fa fa-trash"
+                                                aria-hidden="true"></i></button>
+                                    </div>
                                 </div>
                             </div>
-                            <button type="button" class="remove-bahan btn btn-danger"><i class="fa fa-trash"
-                                    aria-hidden="true"></i></button>
                         </div>
                     @endif
                 </div>
@@ -114,9 +131,32 @@
                     <button type="button" id="add-bahan" class="btn btn-info">Tambah Bahan</button>
                     <button type="submit" class="btn btn-success">Simpan</button>
                 </div>
+
             </div>
         </form>
-
+        <div class="modal fade" id="tambah-satuan">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form id="form-tambah-satuan">
+                            <div class="form-group">
+                                <label for="satuan">Nama Satuan : </label>
+                                <input type="text" id="satuan" class="form-control">
+                            </div>
+                            <div class="modal-footer justify-content-between">
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                <button type="submit" class="btn btn-primary">Tambah</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
 @push('js')
@@ -128,7 +168,7 @@
             });
             $('#add-bahan').click(function() {
                 let newRow = `<div class="row d-flex flex-wrap justify-content-center bahan-row">
-                <div class="col-lg-3 col-sm-1 col-md-1">
+                <div class="col-lg-3 col-sm-1 col-md-3">
                     <div class="form-group">
                         <label for="bahan_id">Bahan : </label>
                         <select class="form-control bahan-select" name="bahan_id[]">
@@ -139,7 +179,7 @@
                         </select>
                     </div>
                 </div>
-                <div class="col-lg-3 col-sm-1 col-md-1">
+                <div class="col-lg-3 col-sm-1 col-md-3">
                     <div class="form-group">
                                         <label for="satuan_bahan">Satuan Bahan : </label>
                                         <select name="satuan_bahan[]" class="form-control" id="">
@@ -152,15 +192,21 @@
 
                                     </div>
                 </div>
-                <div class="col-lg-3 col-sm-1 col-md-1">
-                    <div class="form-group">
-                        <label for="takaran">Takaran: </label>
-                        <input type="text" name="takaran[]" class="form-control" 
-                               placeholder="Masukkan Takaran">
-                    </div>
-                </div>
-                <button type="button" class="remove-bahan btn btn-danger"><i class="fa fa-trash"
-                                    aria-hidden="true"></i></button>
+                  <div class="col-lg-3 col-sm-1 col-md-3">
+                                    <label for="takaran">Takaran: </label>
+                                    <div class="row">
+                                        <div class="col-lg-9 col-md-9">
+                                            <div class="form-group">
+                                        <input type="text" name="takaran[]" class="form-control" 
+                                        placeholder="Masukkan Takaran">
+                                            </div>
+                                        </div>
+                                        <div class="col">
+                                            <button type="button" class="remove-bahan btn btn-danger"><i
+                                                    class="fa fa-trash" aria-hidden="true"></i></button>
+                                        </div>
+                                    </div>
+                                </div>
             </div>`;
                 $('.bahan-container').append(newRow);
                 $('.bahan-select').select2({
@@ -170,6 +216,30 @@
 
             $(document).on('click', '.remove-bahan', function() {
                 $(this).closest('.bahan-row').remove();
+            });
+            $("#form-tambah-satuan").submit((e) => {
+                e.preventDefault();
+                let satuan = $("#satuan").val();
+                if (satuan === "") {
+                    return;
+                }
+                $.ajax({
+                    url: "/dashboard/store",
+                    method: "POST",
+                    headers: {
+                        "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+                    },
+                    data: {
+                        satuan: satuan,
+                    },
+                    success: function(response) {
+                        $("#tambah-satuan").modal("hide");
+                        window.location.reload(true);
+                    },
+                    error: function(error) {
+                        console.log(error.message);
+                    },
+                });
             });
         });
     </script>
